409a1bae5ae76653ef984a8d6bf95fe9e7623233
Abdugani Temirbaev <abdugani.temirbaev@gmail.com>
2021-04-15 09:46:58 +0500

    changes

--- lib/custom_widgets/text_form_field_widget.dart
+++ lib/custom_widgets/text_form_field_widget.dart
@@ -43,102 +43,54 @@ class TextFormFieldWidget extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _TextFormFieldWidgetState createState() => _TextFormFieldWidgetState(
-        borderRadius,
-        enabledBorderColor,
-        focusedBorderColor,
-        hintText,
-        suffixIcon,
-        keyboardType,
-        textInputAction,
-        onSaved,
-        onFieldSubmitted,
-        validator,
-        obscureText,
-        padding,
-        margin,
-        controller,
-        focusNode,
-        onEditingComplete,
-        onChanged,
-        initialValue,
-      );
+  _TextFormFieldWidgetState createState() => _TextFormFieldWidgetState();
 }
 
 class _TextFormFieldWidgetState extends State<TextFormFieldWidget> {
-  final double borderRadius;
-  final Color enabledBorderColor;
-  final Color focusedBorderColor;
-  final String hintText;
-  final IconButton suffixIcon;
-  final TextInputType keyboardType;
-  final TextInputAction textInputAction;
-  final FormFieldSetter<String> onSaved;
-  final ValueChanged<String> onFieldSubmitted;
-  final FormFieldValidator<String> validator;
-  final bool obscureText;
-  final EdgeInsetsGeometry padding;
-  final EdgeInsetsGeometry margin;
-  final TextEditingController controller;
-  final FocusNode focusNode;
-  final VoidCallback onEditingComplete;
-  final ValueChanged<String> onChanged;
-  final String initialValue;
-
-  _TextFormFieldWidgetState(
-    this.borderRadius,
-    this.enabledBorderColor,
-    this.focusedBorderColor,
-    this.hintText,
-    this.suffixIcon,
-    this.keyboardType,
-    this.textInputAction,
-    this.onSaved,
-    this.onFieldSubmitted,
-    this.validator,
-    this.obscureText,
-    this.padding,
-    this.margin,
-    this.controller,
-    this.focusNode,
-    this.onEditingComplete,
-    this.onChanged,
-    this.initialValue,
-  );
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: padding,
-      margin: margin,
+      padding: widget.padding,
+      margin: widget.margin,
       child: TextFormField(
-        decoration: getTextFormFieldDecoration(borderRadius, enabledBorderColor, focusedBorderColor, hintText, suffixIcon),
+        decoration: getTextFormFieldDecoration(widget.borderRadius, widget.enabledBorderColor,
+            widget.focusedBorderColor, widget.hintText, widget.suffixIcon),
         style: TextStyle(fontFamily: 'Montserrat'),
         keyboardType: TextInputType.text,
         textInputAction: TextInputAction.done,
-        obscureText: obscureText,
-        validator: validator,
-        onSaved: onSaved,
-        onFieldSubmitted: onFieldSubmitted,
-        controller: controller,
-        focusNode: focusNode,
-        onEditingComplete: onEditingComplete,
-        onChanged: onChanged,
-        initialValue: initialValue,
+        obscureText: widget.obscureText,
+        validator: widget.validator,
+        onSaved: widget.onSaved,
+        onFieldSubmitted: widget.onFieldSubmitted,
+        controller: widget.controller,
+        focusNode: widget.focusNode,
+        onEditingComplete: widget.onEditingComplete,
+        onChanged: widget.onChanged,
+        initialValue: widget.initialValue,
       ),
     );
   }
 
-  InputDecoration getTextFormFieldDecoration(double borderRadius, Color enabledBorderColor, Color focusedBorderColor, String hintText, IconButton suffixIcon) {
+  InputDecoration getTextFormFieldDecoration(double borderRadius, Color enabledBorderColor,
+      Color focusedBorderColor, String hintText, IconButton suffixIcon) {
     return InputDecoration(
-        enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(borderRadius), borderSide: BorderSide(color: enabledBorderColor, width: 1.0)),
-        focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(borderRadius), borderSide: BorderSide(color: focusedBorderColor, width: 2.0)),
-        errorBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(borderRadius), borderSide: BorderSide(color: Colors.red, width: 1.0)),
-        focusedErrorBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(borderRadius), borderSide: BorderSide(color: Colors.red, width: 2.0)),
-        contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
-        hintText: hintText,
-        hintStyle: TextStyle(fontFamily: 'Montserrat', color: Color(0xFFCDCDCD)),
-        errorStyle: TextStyle(fontSize: 0.1),
-        suffixIcon: suffixIcon);
+      enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+          borderSide: BorderSide(color: enabledBorderColor, width: 1.0)),
+      focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+          borderSide: BorderSide(color: focusedBorderColor, width: 2.0)),
+      errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+          borderSide: BorderSide(color: Colors.red, width: 1.0)),
+      focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+          borderSide: BorderSide(color: Colors.red, width: 2.0)),
+      contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+      hintText: hintText,
+      hintStyle: TextStyle(fontFamily: 'Montserrat', color: Color(0xFFCDCDCD)),
+      errorStyle: TextStyle(fontSize: 0.1),
+      suffixIcon: suffixIcon,
+    );
   }
 }
--- lib/custom_widgets/text_widget.dart
+++ lib/custom_widgets/text_widget.dart
@@ -28,35 +28,27 @@ class TextWidget extends StatefulWidget {
   final FontStyle fontStyle;
 
   @override
-  _TextWidgetState createState() => _TextWidgetState(text, size, weight, color, textAlign, padding, margin, decoration, maxLines, ellipsis, fontStyle);
+  _TextWidgetState createState() => _TextWidgetState();
 }
 
 class _TextWidgetState extends State<TextWidget> {
-  _TextWidgetState(this.text, this.size, this.weight, this.color, this.textAlign, this.padding, this.margin, this.decoration, this.maxLines, this.ellipsis, this.fontStyle);
-
-  final String text;
-  final double size;
-  final FontWeight weight;
-  final Color color;
-  final TextAlign textAlign;
-  final EdgeInsetsGeometry padding;
-  final EdgeInsetsGeometry margin;
-  final TextDecoration decoration;
-  final int maxLines;
-  final bool ellipsis;
-  final FontStyle fontStyle;
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: padding,
-      margin: margin,
+      padding: widget.padding,
+      margin: widget.margin,
       child: Text(
-        text,
-        style: TextStyle(fontFamily: 'Montserrat', fontSize: size, fontWeight: weight, color: color, decoration: decoration, fontStyle: fontStyle),
-        textAlign: textAlign,
-        maxLines: maxLines,
-        overflow: ellipsis ? TextOverflow.ellipsis : null,
+        widget.text,
+        style: TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: widget.size,
+            fontWeight: widget.weight,
+            color: widget.color,
+            decoration: widget.decoration,
+            fontStyle: widget.fontStyle),
+        textAlign: widget.textAlign,
+        maxLines: widget.maxLines,
+        overflow: widget.ellipsis ? TextOverflow.ellipsis : null,
       ),
     );
   }
